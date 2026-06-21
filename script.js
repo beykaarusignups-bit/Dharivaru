5681c6d708ea86954b99ae832e40f46481b904e6
beykaarusignups-bit <beykaarusignups@gmail.com>
2026-06-21 16:01:16 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Student Help Network Loaded");
+function showForm(type){
+
+    let section = document.getElementById("formSection");
+    let title = document.getElementById("formTitle");
+
+    section.classList.remove("hidden");
+
+    if(type === "help"){
+        title.innerHTML = "Tell us what help you need 📚";
+    }
+    else{
+        title.innerHTML = "Tell us how you can help 🤝";
+    }
+
+}
